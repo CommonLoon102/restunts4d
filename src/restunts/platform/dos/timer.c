109e6e560f0282332c128d249082fd90825555bd
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-08-29 06:02:30 +0200

    Port DOS timer interrupt handler to C

--- src/restunts/platform/dos/timer.c
+++ src/restunts/platform/dos/timer.c
@@ -13,13 +13,95 @@ extern legacy_u16 word_3F87C;
 extern legacy_u16 word_3F87E;
 extern legacy_u8 byte_3F880;
 extern legacy_u8 byte_3F881;
+extern legacy_u16 word_3F882;
 extern legacy_u16 word_3F884;
 extern legacy_u16 word_3F886;
+extern legacy_u16 word_3F888;
+extern legacy_u16 word_3F88A;
 extern legacy_u8 byte_3F88C;
+extern legacy_s16 word_3F88E;
 extern void (far* timerintr[6])(void);
 extern interrupt_handler_type dword_3F874;
-extern void interrupt timer_intr_callback(void);
 extern void add_exit_handler(void (far* exit_handler)(void));
+
+static void interrupt dos_timer_interrupt(void);
+
+static void dos_timer_chain_previous_handler(void)
+{
+	__asm {
+		pushf
+		call dword ptr dword_3F874
+	}
+}
+
+static void dos_timer_increment_counter(legacy_u32* counter)
+{
+	legacy_u16* words;
+
+	words = (legacy_u16*)counter;
+	words[0] = (legacy_u16)(words[0] + 1U);
+	if (words[0] == 0)
+		words[1] = (legacy_u16)(words[1] + 1U);
+}
+
+static void interrupt dos_timer_interrupt(void)
+{
+	legacy_u16 callback_index;
+	legacy_s16 reentry_count;
+
+	/* Match the original IRQ0 handler: allow nested interrupts after the
+	 * compiler's interrupt prologue has saved the interrupted registers. */
+	enable();
+	word_3F886 = (legacy_u16)(word_3F886 - 1U);
+	if (LEGACY_S16_FROM_BITS(word_3F886) <= 0) {
+		word_3F87C = (legacy_u16)(word_3F87C + 1U);
+		if (word_3F87C == 0)
+			word_3F87E = (legacy_u16)(word_3F87E + 1U);
+		word_3F886 = word_3F884;
+
+		if (byte_3F881 != 0) {
+			if (byte_3F880 != 0) {
+				word_3F882 = (legacy_u16)(word_3F882 - 1U);
+				if (LEGACY_S16_FROM_BITS(word_3F882) <= 0) {
+					byte_3F880 = 0;
+					byte_3F881 = 0;
+				}
+			}
+			dos_timer_chain_previous_handler();
+		} else {
+			outp(0x20U, 0x20U);
+		}
+	} else {
+		outp(0x20U, 0x20U);
+	}
+
+	if (((legacy_u16)word_3F88E & 0x00FFU) != 0)
+		goto callbacks_finished;
+
+	dos_timer_increment_counter(&timer_callback_counter);
+	disable();
+	if (byte_3F88C == 0) {
+		byte_3F88C = 1U;
+		enable();
+		for (callback_index = 0; callback_index < 6U;
+			callback_index++) {
+			if (FP_SEG(timerintr[callback_index]) == 0U)
+				break;
+			timerintr[callback_index]();
+		}
+		goto callbacks_finished;
+	}
+
+	word_3F88A = (legacy_u16)(word_3F88A + 1U);
+	reentry_count = LEGACY_S16_FROM_BITS(word_3F88A);
+	if (LEGACY_S16_FROM_BITS(word_3F888) < reentry_count)
+		word_3F888 = word_3F88A;
+	return;
+
+callbacks_finished:
+	byte_3F88C = 0;
+	word_3F88A = 0;
+}
 
 static void dos_timer_write_vector(interrupt_handler_type handler)
 {
@@ -48,7 +130,7 @@ void dos_timer_shutdown(void)
 	legacy_u8 interrupt_mask;
 
 	installed_handler = getvect(8);
-	if (installed_handler != timer_intr_callback)
+	if (installed_handler != dos_timer_interrupt)
 		return;
 
 	interrupt_mask = (legacy_u8)inp(0x21U);
@@ -82,9 +164,9 @@ void dos_timer_setup_interrupt(void)
 	outp(0x21U, interrupt_mask | 3U);
 
 	installed_handler = getvect(8);
-	if (installed_handler != timer_intr_callback) {
+	if (installed_handler != dos_timer_interrupt) {
 		dword_3F874 = installed_handler;
-		dos_timer_write_vector(timer_intr_callback);
+		dos_timer_write_vector(dos_timer_interrupt);
 	}
 
 	interrupt_mask = (legacy_u8)inp(0x21U);
