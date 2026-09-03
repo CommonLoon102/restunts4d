9df91586e14a200d42fc507d6007b2c3b59aa446
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-03 13:39:35 +0200

    Refactor: centralize race resource declarations

--- src/restunts/c/race_resources.c
+++ src/restunts/c/race_resources.c
@@ -4,27 +4,13 @@
 #include "memmgr.h"
 #include "menu_internal.h"
 #include "race_resources.h"
+#include "race_resources_internal.h"
 #include "replay_viewer.h"
 #include "shape2d.h"
 #include "shape3d.h"
 #include "ui_dialog.h"
 
-extern legacy_s8 byte_3B8F6;
-extern legacy_s8 far* skybox_res_ofs;
-extern legacy_s8 far* sdgame2ptr;
-extern legacy_s16 sdgame2_widths[];
-extern legacy_s8 far* sdgame2shapes[];
-extern legacy_s8 byte_46167;
-extern legacy_u16 skybox_ptr1;
-extern legacy_u16 skybox_ptr2;
-extern legacy_u16 skybox_ptr3;
-extern legacy_u16 skybox_ptr4;
-extern legacy_u16 skybox_current;
-extern legacy_u16 word_454CE;
-extern legacy_s16 skybox_sky_color;
-extern legacy_s16 skybox_wat_color;
 extern legacy_s16 meter_needle_color;
-extern legacy_s8 far* skyboxes[];
 extern void far* engptr;
 extern void far* eng1ptr;
 extern void far* fontledresptr;
---
+++ src/restunts/c/race_resources_internal.h
@@ -0,0 +1,22 @@
+#ifndef RESTUNTS_RACE_RESOURCES_INTERNAL_H
+#define RESTUNTS_RACE_RESOURCES_INTERNAL_H
+
+#include "legacy.h"
+
+extern legacy_s8 byte_3B8F6;
+extern legacy_s8 far* skybox_res_ofs;
+extern legacy_s8 far* sdgame2ptr;
+extern legacy_s16 sdgame2_widths[];
+extern legacy_s8 far* sdgame2shapes[];
+extern legacy_s8 byte_46167;
+extern legacy_u16 skybox_ptr1;
+extern legacy_u16 skybox_ptr2;
+extern legacy_u16 skybox_ptr3;
+extern legacy_u16 skybox_ptr4;
+extern legacy_u16 skybox_current;
+extern legacy_u16 word_454CE;
+extern legacy_s16 skybox_sky_color;
+extern legacy_s16 skybox_wat_color;
+extern legacy_s8 far* skyboxes[];
+
+#endif
