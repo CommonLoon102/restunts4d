3298314aa8459cb65e27306bf5405f9464ac4060
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-03 13:39:35 +0200

    Refactor: centralize race resource declarations

--- src/restunts/c/restunts.c
+++ src/restunts/c/restunts.c
@@ -15,6 +15,7 @@
 #include "platform.h"
 #include "race.h"
 #include "race_resources.h"
+#include "race_resources_internal.h"
 #include "replay_record.h"
 #include "replay_viewer.h"
 #include "replay_viewer_internal.h"
@@ -114,23 +115,8 @@ extern legacy_s16 polyinfonumpolys;
 extern legacy_u8 far* polyinfoptrs[]; // array size = 0x190
 extern legacy_u16 poly_linked_list_40ED6[]; // array size = 0x190
 
-extern legacy_s8 byte_3B8F6;
-extern legacy_s8 far* skybox_res_ofs;
-extern legacy_s8 far* sdgame2ptr;
-extern legacy_s16 sdgame2_widths[];
-extern legacy_s8 far* sdgame2shapes[];
-extern legacy_s8 byte_46167;
-extern legacy_u16 skybox_ptr1;
-extern legacy_u16 skybox_ptr2;
-extern legacy_u16 skybox_ptr3;
-extern legacy_u16 skybox_ptr4;
-extern legacy_u16 skybox_current;
-extern legacy_u16 word_454CE;
-extern legacy_s16 skybox_sky_color;
 extern legacy_s16 skybox_grd_color;
-extern legacy_s16 skybox_wat_color;
 extern legacy_s16 dialog_fnt_colour;
-extern legacy_s8 far* skyboxes[];
 
 
 extern legacy_s16 font_op2(const legacy_s8* text);
