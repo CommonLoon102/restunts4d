b562c824f7f8ef187c001ae11722c394baf842bf
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-06 00:09:46 +0200

    Name opponent route and input states

--- src/restunts/c/opponent.c
+++ src/restunts/c/opponent.c
@@ -1,4 +1,5 @@
 #include "state_internal.h"
+#include "game_input.h"
 
 #define LEGACY_NULL_TRACK_VECTOR_CAPACITY 256
 #define LEGACY_NULL_TRACK_TEXT_SPACE 32
@@ -11,6 +12,7 @@
 #define OPPONENT_NORMAL_SPEED_STEP 1
 #define OPPONENT_LOW_RATE_SPEED_STEP 2
 #define OPPONENT_ROUTE_REACHED_DISTANCE 200
+#define OPPONENT_ROUTE_HEIGHT_UNSPECIFIED (-1)
 #define OPPONENT_PLAYER_HEIGHT_RANGE 90
 #define OPPONENT_PLAYER_LATERAL_RANGE 180
 #define OPPONENT_PLAYER_FORWARD_RANGE 600
@@ -266,7 +268,7 @@ void opponent_op(void)
 			state.opponentstate.field_CF = 0;
 	} else {
 	route_target = state.opponentstate.car_vec_unk3;
-	if (route_target.y != -1) {
+	if (route_target.y != OPPONENT_ROUTE_HEIGHT_UNSPECIFIED) {
 		relative.x = LEGACY_S16_WRAP_SUB(route_target.x, opponent_x);
 		relative.y = LEGACY_S16_WRAP_SUB(route_target.y, opponent_y);
 		relative.z = LEGACY_S16_WRAP_SUB(route_target.z, opponent_z);
@@ -297,7 +299,8 @@ void opponent_op(void)
 			transformed.z >= OPPONENT_PLAYER_REAR_RANGE) {
 			relative.x = LEGACY_S16_WRAP_SUB(
 				player_x, state.opponentstate.car_vec_unk3.x);
-			relative.y = state.opponentstate.car_vec_unk3.y == -1 ? 0 :
+			relative.y = state.opponentstate.car_vec_unk3.y ==
+				OPPONENT_ROUTE_HEIGHT_UNSPECIFIED ? 0 :
 				LEGACY_S16_WRAP_SUB(player_y,
 					state.opponentstate.car_vec_unk3.y);
 			relative.z = LEGACY_S16_WRAP_SUB(
@@ -307,8 +310,9 @@ void opponent_op(void)
 				route_target.x = opponent_average(
 					state.opponentstate.car_vec_unk3.x,
 					state.opponentstate.car_vec_unk5.x);
-				route_target.y = state.opponentstate.car_vec_unk3.y == -1 ?
-					-1 : opponent_average(
+				route_target.y = state.opponentstate.car_vec_unk3.y ==
+					OPPONENT_ROUTE_HEIGHT_UNSPECIFIED ?
+					OPPONENT_ROUTE_HEIGHT_UNSPECIFIED : opponent_average(
 						state.opponentstate.car_vec_unk3.y,
 						state.opponentstate.car_vec_unk5.y);
 				route_target.z = opponent_average(
@@ -322,8 +326,9 @@ void opponent_op(void)
 				route_target.x = opponent_average(
 					state.opponentstate.car_vec_unk3.x,
 					state.opponentstate.car_vec_unk4.x);
-				route_target.y = state.opponentstate.car_vec_unk3.y == -1 ?
-					-1 : opponent_average(
+				route_target.y = state.opponentstate.car_vec_unk3.y ==
+					OPPONENT_ROUTE_HEIGHT_UNSPECIFIED ?
+					OPPONENT_ROUTE_HEIGHT_UNSPECIFIED : opponent_average(
 						state.opponentstate.car_vec_unk3.y,
 						state.opponentstate.car_vec_unk4.y);
 				route_target.z = opponent_average(
@@ -338,7 +343,7 @@ void opponent_op(void)
 	}
 
 	relative.x = LEGACY_S16_WRAP_SUB(route_target.x, opponent_x);
-	relative.y = route_target.y == -1 ? 0 :
+	relative.y = route_target.y == OPPONENT_ROUTE_HEIGHT_UNSPECIFIED ? 0 :
 		LEGACY_S16_WRAP_SUB(route_target.y, opponent_y);
 	relative.z = LEGACY_S16_WRAP_SUB(route_target.z, opponent_z);
 	mat_mul_vector(&relative, rotation, &transformed);
@@ -391,7 +396,7 @@ void opponent_op(void)
 	input = 0;
 	if (state.opponentstate.car_sumSurfRearWheels != 0) {
 		if (state.opponentstate.car_crashBmpFlag != 0) {
-			input = 2;
+			input = INPUT_BRAKE_FLAG;
 		} else if (state.opponentstate.car_36MwhlAngle != 0) {
 			speed_threshold = LEGACY_U16_SHL(speed_step,
 				OPPONENT_COAST_SPEED_SHIFT);
@@ -411,14 +416,14 @@ void opponent_op(void)
 			if (LEGACY_U16_WRAP_SUB(target_speed,
 				OPPONENT_ACCELERATE_MARGIN) >
 				state.opponentstate.car_speed) {
-				input = 1;
+				input = INPUT_ACCELERATE_FLAG;
 			} else if (LEGACY_U16_WRAP_ADD(target_speed,
 				OPPONENT_BRAKE_MARGIN) <
 				state.opponentstate.car_speed) {
-				input = 2;
+				input = INPUT_BRAKE_FLAG;
 			}
 		} else {
-			input = 2;
+			input = INPUT_BRAKE_FLAG;
 		}
 	}
 
@@ -655,7 +660,7 @@ legacy_s16 sub_18D60(
 
 	column = (legacy_u8)td21_col_from_path[track_index];
 	row = (legacy_u8)td22_row_from_path[track_index];
-	if (first_point.y != -1 &&
+	if (first_point.y != OPPONENT_ROUTE_HEIGHT_UNSPECIFIED &&
 		td15_terr_map_main[terrainrows[row] + column] == 6) {
 		first_point.y = LEGACY_S16_WRAP_ADD(
 			first_point.y, hillHeightConsts[1]);
@@ -672,8 +677,8 @@ legacy_s16 sub_18D60(
 	second_point.x = LEGACY_S16_WRAP_ADD(second_point.x, base_position);
 
 	output[0].x = route_average(first_point.x, second_point.x);
-	if (first_point.y == -1)
-		output[0].y = -1;
+	if (first_point.y == OPPONENT_ROUTE_HEIGHT_UNSPECIFIED)
+		output[0].y = OPPONENT_ROUTE_HEIGHT_UNSPECIFIED;
 	else
 		output[0].y = route_average(first_point.y, second_point.y);
 	output[0].z = route_average(first_point.z, second_point.z);
