f7698958601653bccdbdc6a18cd1cd923ca29ba7
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-05 20:24:49 +0200

    Name DOS mouse driver constants

--- src/restunts/platform/dos/mouse.c
+++ src/restunts/platform/dos/mouse.c
@@ -2,6 +2,20 @@
 #include <dos.h>
 #include "../../c/platform.h"
 
+#define DOS_MOUSE_INTERRUPT 51
+#define DOS_MOUSE_BIOS_INTERRUPT 21
+#define DOS_MOUSE_BIOS_DISABLE_POINTING_DEVICE 49665U
+#define DOS_MOUSE_RESET_FUNCTION 0U
+#define DOS_MOUSE_GET_STATE_FUNCTION 3U
+#define DOS_MOUSE_SET_POSITION_FUNCTION 4U
+#define DOS_MOUSE_SET_HORIZONTAL_RANGE_FUNCTION 7U
+#define DOS_MOUSE_SET_VERTICAL_RANGE_FUNCTION 8U
+#define DOS_MOUSE_SET_PIXEL_RATIO_FUNCTION 15U
+#define DOS_MOUSE_DOUBLE_WIDTH 320
+#define DOS_MOUSE_DOUBLE_WIDTH_SCALE 1U
+#define DOS_MOUSE_DEFAULT_PIXEL_RATIO 16U
+#define DOS_MOUSE_AVAILABLE LEGACY_U16_MAX
+
 static legacy_u16 dos_mouse_button_count;
 
 static legacy_u8 dos_mouse_horizontal_scale;
@@ -14,10 +28,10 @@ static void dos_mouse_set_pixel_ratio(legacy_u16 horizontal,
 {
 	union REGS registers;
 
-	registers.x.ax = 0x000FU;
+	registers.x.ax = DOS_MOUSE_SET_PIXEL_RATIO_FUNCTION;
 	registers.x.cx = horizontal;
 	registers.x.dx = vertical;
-	int86(0x33, &registers, &registers);
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 }
 
 void dos_mouse_set_minmax(legacy_s16 minimum_x, legacy_s16 minimum_y,
@@ -27,15 +41,15 @@ void dos_mouse_set_minmax(legacy_s16 minimum_x, legacy_s16 minimum_y,
 	legacy_u16 scale;
 
 	scale = dos_mouse_horizontal_scale;
-	registers.x.ax = 7;
+	registers.x.ax = DOS_MOUSE_SET_HORIZONTAL_RANGE_FUNCTION;
 	registers.x.cx = (legacy_u16)minimum_x << scale;
 	registers.x.dx = (legacy_u16)maximum_x << scale;
-	int86(0x33, &registers, &registers);
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 
-	registers.x.ax = 8;
+	registers.x.ax = DOS_MOUSE_SET_VERTICAL_RANGE_FUNCTION;
 	registers.x.cx = (legacy_u16)minimum_y;
 	registers.x.dx = (legacy_u16)maximum_y;
-	int86(0x33, &registers, &registers);
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 }
 
 legacy_s16 dos_mouse_init(legacy_s16 width, legacy_s16 height)
@@ -51,20 +65,22 @@ legacy_s16 dos_mouse_init(legacy_s16 width, legacy_s16 height)
 	registers.x.di = 0;
 	registers.x.cflag = 0;
 	registers.x.flags = 0;
-	registers.x.ax = 0xC201U;
-	int86(0x15, &registers, &registers);
+	registers.x.ax = DOS_MOUSE_BIOS_DISABLE_POINTING_DEVICE;
+	int86(DOS_MOUSE_BIOS_INTERRUPT, &registers, &registers);
 
-	registers.x.ax = 0;
-	int86(0x33, &registers, &registers);
+	registers.x.ax = DOS_MOUSE_RESET_FUNCTION;
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 	installed = (legacy_s16)registers.x.ax;
 	dos_mouse_button_count = registers.x.bx;
 	if (installed != 0) {
-		dos_mouse_horizontal_scale = width == 0x140 ? 1U : 0U;
+		dos_mouse_horizontal_scale = width == DOS_MOUSE_DOUBLE_WIDTH ?
+			DOS_MOUSE_DOUBLE_WIDTH_SCALE : 0U;
 		dos_mouse_set_minmax(0, 0,
 			LEGACY_S16_WRAP_SUB(width, 1),
 			LEGACY_S16_WRAP_SUB(height, 1));
-		dos_mouse_set_pixel_ratio(0x10U, 0x10U);
-		dos_mouse_available = 0xFFFFU;
+		dos_mouse_set_pixel_ratio(DOS_MOUSE_DEFAULT_PIXEL_RATIO,
+			DOS_MOUSE_DEFAULT_PIXEL_RATIO);
+		dos_mouse_available = DOS_MOUSE_AVAILABLE;
 	}
 	return installed;
 }
@@ -73,20 +89,20 @@ void dos_mouse_set_position(legacy_s16 x, legacy_s16 y)
 {
 	union REGS registers;
 
-	registers.x.ax = 4;
+	registers.x.ax = DOS_MOUSE_SET_POSITION_FUNCTION;
 	dos_mouse_last_x = (legacy_u16)x;
 	registers.x.cx = (legacy_u16)x << dos_mouse_horizontal_scale;
 	registers.x.dx = (legacy_u16)y;
 	dos_mouse_last_y = (legacy_u16)y;
-	int86(0x33, &registers, &registers);
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 }
 
 void dos_mouse_get_state(legacy_s16* buttons, legacy_s16* x, legacy_s16* y)
 {
 	union REGS registers;
 
-	registers.x.ax = 3;
-	int86(0x33, &registers, &registers);
+	registers.x.ax = DOS_MOUSE_GET_STATE_FUNCTION;
+	int86(DOS_MOUSE_INTERRUPT, &registers, &registers);
 	*buttons = (legacy_s16)registers.x.bx;
 	*x = (legacy_s16)(registers.x.cx >> dos_mouse_horizontal_scale);
 	*y = (legacy_s16)registers.x.dx;
