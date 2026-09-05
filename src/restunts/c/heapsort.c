87b42a32a646bf840955b01d7f4c1aecb3e3e348
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-05 22:31:43 +0200

    Name heap sort gap divisor

--- src/restunts/c/heapsort.c
+++ src/restunts/c/heapsort.c
@@ -1,12 +1,14 @@
 #include "legacy.h"
 
+#define HEAPSORT_GAP_DIVISOR 2
+
 void heapsort_by_order(legacy_s16 count, legacy_s16* values, legacy_s16* order) {
 	legacy_s16 gap;
 	legacy_s16 counter;
 	legacy_s16 index;
 	legacy_s16 temp;
 
-	gap = LEGACY_S16_DIV_OR_ZERO(count, 2);
+	gap = LEGACY_S16_DIV_OR_ZERO(count, HEAPSORT_GAP_DIVISOR);
 	while (gap > 0) {
 		counter = gap;
 		while (counter < count) {
@@ -23,6 +25,6 @@ void heapsort_by_order(legacy_s16 count, legacy_s16* values, legacy_s16* order) 
 			}
 			counter++;
 		}
-		gap = LEGACY_S16_DIV_OR_ZERO(gap, 2);
+		gap = LEGACY_S16_DIV_OR_ZERO(gap, HEAPSORT_GAP_DIVISOR);
 	}
 }
