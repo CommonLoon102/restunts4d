a12ba1e14a303d0b8b7f79f79224052d7a3f00cf
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-06 04:23:22 +0200

    Name headless runtime states

--- src/restunts/c/headless.c
+++ src/restunts/c/headless.c
@@ -3,6 +3,14 @@
 #include "memmgr.h"
 #include "platform.h"
 
+#define HEADLESS_STRING_INDEX_FIRST 0U
+#define HEADLESS_STRING_TERMINATOR 0
+#define HEADLESS_FORMAT_NEXT_CHARACTER_OFFSET 1U
+#define HEADLESS_FATAL_EXIT_STATUS 1
+#define HEADLESS_VIDEO_DISABLED 0
+#define HEADLESS_VIDEO_UNIT_SCALE 1
+#define HEADLESS_KEY_NONE 0
+
 extern void headless_exit(legacy_s16 result);
 
 void add_exit_handler(void (far* exit_handler)(void))
@@ -14,8 +22,8 @@ static void headless_write_text(const legacy_s8* text)
 {
 	legacy_u16 length;
 
-	length = 0;
-	while (text[length] != 0)
+	length = HEADLESS_STRING_INDEX_FIRST;
+	while (text[length] != HEADLESS_STRING_TERMINATOR)
 		length++;
 	(void)dos_write_stdout(text, length);
 }
@@ -27,8 +35,10 @@ void fatal_error(const legacy_s8* format, ...)
 	const legacy_s8* detail;
 
 	headless_write_text(format);
-	for (index = 0; format[index] != 0; index++) {
-		if (format[index] == '%' && format[index + 1] == 's') {
+	for (index = HEADLESS_STRING_INDEX_FIRST;
+		format[index] != HEADLESS_STRING_TERMINATOR; index++) {
+		if (format[index] == '%' &&
+			format[index + HEADLESS_FORMAT_NEXT_CHARACTER_OFFSET] == 's') {
 			va_start(arguments, format);
 			detail = va_arg(arguments, const legacy_s8*);
 			va_end(arguments);
@@ -38,7 +48,7 @@ void fatal_error(const legacy_s8* format, ...)
 		}
 	}
 	headless_write_text("\r\n");
-	headless_exit(1);
+	headless_exit(HEADLESS_FATAL_EXIT_STATUS);
 }
 
 void init_div0(void)
@@ -51,8 +61,8 @@ void init_main(legacy_s16 argc, legacy_s8* argv[])
 	(void)argc;
 	(void)argv;
 	init_video_geometry_flags();
-	video_flag5_is0 = 0;
-	video_flag6_is1 = 1;
+	video_flag5_is0 = HEADLESS_VIDEO_DISABLED;
+	video_flag6_is1 = HEADLESS_VIDEO_UNIT_SCALE;
 	textresprefix = 'e';
 	framespersec = GAME_FRAME_RATE_NORMAL;
 	mmgr_alloc_a000();
@@ -61,5 +71,5 @@ void init_main(legacy_s16 argc, legacy_s8* argv[])
 
 legacy_s16 kb_read_char(void)
 {
-	return 0;
+	return HEADLESS_KEY_NONE;
 }
