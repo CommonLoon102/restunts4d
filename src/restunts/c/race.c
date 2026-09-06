d825d670fe5b1ce7cf1469b596e25ce46f948698
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-06 06:50:54 +0200

    Share replay exit request state

--- src/restunts/c/race.c
+++ src/restunts/c/race.c
@@ -318,7 +318,8 @@ void run_game(void) {
 				if (byte_449DA != 0) {
 
 					if ((game_replay_mode != REPLAY_MODE_LIVE ||
-						state.game_3F6autoLoadEvalFlag == 4) && byte_449DA != 2) {
+						state.game_3F6autoLoadEvalFlag == 4) &&
+						byte_449DA != REPLAY_EXIT_REQUESTED) {
 						byte_449DA = 0;
 						game_replay_mode = REPLAY_MODE_PLAYBACK;
 						mouse_minmax_position(0);
