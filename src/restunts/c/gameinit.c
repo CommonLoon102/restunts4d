0dd4475d133a7895271daa957c55e58a40a24639
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-06 03:03:16 +0200

    Name initial car grip demand

--- src/restunts/c/gameinit.c
+++ src/restunts/c/gameinit.c
@@ -11,6 +11,7 @@
 #define CAR_INITIAL_SURFACE_GRIP 1000
 #define CAR_PSEUDO_GRAVITY_NONE 0
 #define CAR_INITIAL_SPEED_DIFFERENCE 0
+#define CAR_INITIAL_DEMANDED_GRIP 0
 #define GAMESTATE_CHECKPOINT_INTERVAL_SECONDS 30U
 #define TIMER_TICKS_PER_SECOND 100U
 #define INITIAL_CAMERA_LATERAL_OFFSET 512
@@ -118,7 +119,7 @@ void init_carstate_from_simd(struct CARSTATE* playerstate, struct SIMD* simd,
 	playerstate->car_sumSurfFrontWheels = CAR_WHEELS_PER_AXLE;
 	playerstate->car_sumSurfRearWheels = CAR_WHEELS_PER_AXLE;
 	playerstate->car_sumSurfAllWheels = CARSTATE_WHEEL_COUNT;
-	playerstate->car_demandedGrip = 0;
+	playerstate->car_demandedGrip = CAR_INITIAL_DEMANDED_GRIP;
 	playerstate->car_surfacegrip_sum = CAR_INITIAL_SURFACE_GRIP;
 
 	whlPos.x = LEGACY_S16_FROM_BITS((legacy_u16)
