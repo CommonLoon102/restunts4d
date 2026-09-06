bbc517b0b1a361e79b0105fd97774fd7d85b5f9f
CommonLoon102 <321850+CommonLoon102@users.noreply.github.com>
2026-09-06 05:05:10 +0200

    Name heapsort termination bounds

--- src/restunts/c/heapsort.c
+++ src/restunts/c/heapsort.c
@@ -1,6 +1,8 @@
 #include "legacy.h"
 
 #define HEAPSORT_GAP_DIVISOR 2
+#define HEAPSORT_NO_GAP 0
+#define HEAPSORT_FIRST_INDEX 0
 
 void heapsort_by_order(legacy_s16 count, legacy_s16* values, legacy_s16* order) {
 	legacy_s16 gap;
@@ -9,11 +11,12 @@ void heapsort_by_order(legacy_s16 count, legacy_s16* values, legacy_s16* order) 
 	legacy_s16 temp;
 
 	gap = LEGACY_S16_DIV_OR_ZERO(count, HEAPSORT_GAP_DIVISOR);
-	while (gap > 0) {
+	while (gap > HEAPSORT_NO_GAP) {
 		counter = gap;
 		while (counter < count) {
 			index = counter - gap;
-			while (index >= 0 && values[index + gap] > values[index]) {
+			while (index >= HEAPSORT_FIRST_INDEX &&
+				values[index + gap] > values[index]) {
 				temp = values[index];
 				values[index] = values[index + gap];
 				values[index + gap] = temp;
